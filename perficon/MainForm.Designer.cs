20453315e93660303fa1c008cd5ef51fe4f26f5c
Gernot Lenkner <g.lenkner@outlook.de>
2019-01-03 14:16:59 +0100

    initial commit

---
+++ perficon/MainForm.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace PerfIcon
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tMain = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hideOnMinimizeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.showToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.contextMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tMain
+            // 
+            this.tMain.Tick += new System.EventHandler(this.tMain_Tick);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.ContextMenuStrip = this.contextMenuStrip;
+            this.notifyIcon.Text = "notifyIcon1";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseDoubleClick);
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showToolStripMenuItem,
+            this.hideOnMinimizeToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.closeToolStripMenuItem});
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.Size = new System.Drawing.Size(169, 76);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(168, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // hideOnMinimizeToolStripMenuItem
+            // 
+            this.hideOnMinimizeToolStripMenuItem.Checked = true;
+            this.hideOnMinimizeToolStripMenuItem.CheckOnClick = true;
+            this.hideOnMinimizeToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.hideOnMinimizeToolStripMenuItem.Name = "hideOnMinimizeToolStripMenuItem";
+            this.hideOnMinimizeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.hideOnMinimizeToolStripMenuItem.Text = "Hide on Minimize";
+            this.hideOnMinimizeToolStripMenuItem.Click += new System.EventHandler(this.hideOnMinimizeToolStripMenuItem_Click);
+            // 
+            // showToolStripMenuItem
+            // 
+            this.showToolStripMenuItem.Name = "showToolStripMenuItem";
+            this.showToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.showToolStripMenuItem.Text = "Show";
+            this.showToolStripMenuItem.Click += new System.EventHandler(this.showToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(247, 218);
+            this.Name = "MainForm";
+            this.Text = "PerfIcon";
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            this.contextMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer tMain;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hideOnMinimizeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem showToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+    }
+}
+
---
+++ perficon/MainForm.cs
@@ -0,0 +1,118 @@
+﻿using Cave.Collections;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PerfIcon
+{
+    public partial class MainForm : Form
+    {
+        Stopwatch SwTimer = new Stopwatch();
+        PerformanceCounter PCPhysicalDisk = null;
+        MovingAverageFloat MAPhysicalDisk = new MovingAverageFloat();
+        MovingAverageFloat MAGraph = new MovingAverageFloat();
+
+        Random TestRandom = new Random();
+
+        public MainForm()
+        {
+            InitializeComponent();
+            MAPhysicalDisk.MaximumCount = 10;
+            MAGraph.MaximumCount = 30;
+            try
+            {
+                PCPhysicalDisk = new PerformanceCounter("PhysicalDisk", "% Disk Time", "_Total");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Could not open Performance Counter. Please check your rights.");
+                throw ex;
+            }
+
+            DrawGraph();
+            notifyIcon.Visible = true;
+            tMain.Enabled = true;
+            SwTimer.Restart();
+        }
+
+        private void tMain_Tick(object sender, EventArgs e)
+        {
+            // get Performace, calculate Average
+            MAPhysicalDisk.Add(PCPhysicalDisk.NextValue());
+            if (SwTimer.ElapsedMilliseconds > 1000)
+            {
+                SwTimer.Reset();
+                MAGraph.Add(MAPhysicalDisk.Average);
+                DrawGraph();
+                SwTimer.Start();
+            }
+        }
+
+
+        private void DrawGraph()
+        {
+            Bitmap gBitmap = new Bitmap(32, 32);
+
+            using (var graphics = Graphics.FromImage(gBitmap))
+            {
+                graphics.Clear(Color.Black);
+                graphics.DrawRectangle(Pens.Gray, 0,0,31,31);
+                int posX = 31 - MAGraph.Count;
+                foreach (float value in MAGraph)
+                {
+                    if (posX > 30) { break; }
+                    // clamp value
+                    int y = (int)Math.Min(29f, 30 * value / 100f);
+                    graphics.DrawLine(Pens.Green, posX, 30 - y, posX, 30);
+                    posX++;
+                }
+
+            }
+
+            IntPtr HICon = gBitmap.GetHicon();
+            notifyIcon.Icon = Icon.FromHandle(HICon); ;
+            notifyIcon.Text = String.Format("HDD:{0:0.##}%", MAPhysicalDisk.Average);
+
+
+            using (var graphics = this.CreateGraphics())
+            {
+                graphics.DrawImage(gBitmap, ClientRectangle);
+            }
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                this.Visible = !hideOnMinimizeToolStripMenuItem.Checked;
+            }
+            else
+            {
+                this.Visible = true;
+            }
+        }
+
+        private void hideOnMinimizeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MainForm_Resize(sender, e);
+        }
+
+        private void showToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Visible = true;
+            WindowState = FormWindowState.Normal;
+            this.BringToFront();
+        }
+
+        private void notifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            showToolStripMenuItem_Click(sender, e);
+        }
+    }
+}
