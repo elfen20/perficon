4fbcdb5f4856e398cbbd1e174095a09d4642cea7
Gernot Lenkner <g.lenkner@outlook.de>
2019-01-04 09:05:25 +0100

    + fixed gdi handle leak + comments - removed test random

--- perficon/MainForm.cs
+++ perficon/MainForm.cs
@@ -2,22 +2,51 @@
 using System;
 using System.Diagnostics;
 using System.Drawing;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace PerfIcon
 {
+    /// <summary>
+    /// Main Form, shows the Graph if visible
+    /// </summary>
     public partial class MainForm : Form
     {
+        /// <summary>
+        /// Stopwatch for checking when to redraw the graph
+        /// </summary>
         Stopwatch SwTimer = new Stopwatch();
+
+        /// <summary>
+        /// Performance counter for physical disk activity
+        /// </summary>
         PerformanceCounter PCPhysicalDisk = null;
+
+        /// <summary>
+        /// holds first layer of average data of physical disk activity
+        /// </summary>
         MovingAverageFloat MAPhysicalDisk = new MovingAverageFloat();
+
+        /// <summary>
+        /// holds second layer of data for drawing the graph
+        /// </summary>
         MovingAverageFloat MAGraph = new MovingAverageFloat();
 
-        Random TestRandom = new Random();
+        /// <summary>
+        /// Destroys a Icon Handle. Needs to be imported.
+        /// </summary>
+        /// <param name="handle"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        extern static bool DestroyIcon(IntPtr handle);
 
+        /// <summary>
+        /// initializes the main form
+        /// </summary>
         public MainForm()
         {
             InitializeComponent();
+            this.Visible = false;
             MAPhysicalDisk.MaximumCount = 10;
             MAGraph.MaximumCount = 30;
             try
@@ -32,14 +61,18 @@ namespace PerfIcon
 
             DrawGraph();
             notifyIcon.Visible = true;            
-            tMain.Enabled = true;
+            timerMain.Enabled = true;
             SwTimer.Restart();
-            WindowState = FormWindowState.Minimized;
         }
 
-        private void TMain_Tick(object sender, EventArgs e)
+        /// <summary>
+        /// main timer tick. gets performance counter data, calculates the average and redraws the graph if necessary
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TimerMain_Tick(object sender, EventArgs e)
         {
-            // get Performace, calculate Average
+            // get Performace, calculate average
             MAPhysicalDisk.Add(PCPhysicalDisk.NextValue());
             if (SwTimer.ElapsedMilliseconds > 1000)
             {
@@ -50,13 +83,16 @@ namespace PerfIcon
             }
         }
 
-
+        /// <summary>
+        /// draws the graph on the notify icon and on the form if visible
+        /// </summary>
         private void DrawGraph()
         {
             Bitmap gBitmap = new Bitmap(32, 32);
 
             using (var graphics = Graphics.FromImage(gBitmap))
             {
+                // black background with light gray border
                 graphics.Clear(Color.Black);
                 graphics.DrawRectangle(Pens.LightGray, 0, 0, 31, 31);
                 int posX = 31 - MAGraph.Count;
@@ -71,21 +107,41 @@ namespace PerfIcon
 
             }
 
+            // hold old icon handle
+            IntPtr HOldIcon = notifyIcon.Icon?.Handle ?? IntPtr.Zero;
             IntPtr HICon = gBitmap.GetHicon();
+
+            // set new icon
             notifyIcon.Icon = Icon.FromHandle(HICon); ;
             notifyIcon.Text = String.Format("HDD: {0:0.##}%", MAPhysicalDisk.Average);
 
-            using (var graphics = this.CreateGraphics())
+            // destroy old icon
+            DestroyIcon(HOldIcon);
+
+            if (this.Visible)
             {
-                graphics.DrawImage(gBitmap, ClientRectangle);
+                using (var graphics = this.CreateGraphics())
+                {
+                    graphics.DrawImage(gBitmap, ClientRectangle);
+                }
             }
         }
 
+        /// <summary>
+        /// context menu item to close the form
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void CloseToolStripMenuItem_Click(object sender, EventArgs e)
         {
             this.Close();
         }
 
+        /// <summary>
+        /// hides the form when minimizing if context menu item is set
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void MainForm_Resize(object sender, EventArgs e)
         {
             if (WindowState == FormWindowState.Minimized)
@@ -98,11 +154,21 @@ namespace PerfIcon
             }
         }
 
+        /// <summary>
+        /// hides / shows the form if context menu item is set
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void HideOnMinimizeToolStripMenuItem_Click(object sender, EventArgs e)
         {
             MainForm_Resize(sender, e);
         }
 
+        /// <summary>
+        /// shows the form when context menu item is clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void ShowToolStripMenuItem_Click(object sender, EventArgs e)
         {
             this.Visible = true;
@@ -110,6 +176,11 @@ namespace PerfIcon
             this.BringToFront();
         }
 
+        /// <summary>
+        /// shows the form when notify icon is double clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void NotifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
         {
             ShowToolStripMenuItem_Click(sender, e);
